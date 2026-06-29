650986c7e67fd3b4cc3f38802a8f29d757bfd159
akshitatoluva <akshita.toluva@zemosolabs.com>
2026-06-29 23:37:13 +0530

    feat: Day 2 - JWT authentication with 3-role system (PATIENT, DOCTOR, ADMIN)

---
+++ src/main/java/com/mediqueue/mediqueue/service/AuthService.java
@@ -0,0 +1,54 @@
+package com.mediqueue.mediqueue.service;
+
+import com.mediqueue.mediqueue.dto.AuthResponse;
+import com.mediqueue.mediqueue.dto.LoginRequest;
+import com.mediqueue.mediqueue.dto.RegisterRequest;
+import com.mediqueue.mediqueue.entity.User;
+import com.mediqueue.mediqueue.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import com.mediqueue.mediqueue.util.JwtUtil;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponse register(RegisterRequest request)
+    {
+        if(userRepository.existsByEmail(request.getEmail())){
+            throw new RuntimeException("Email already registered");
+        }
+
+        User user= User.builder()
+                .name(request.getName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(request.getRole())
+                .build();
+
+        userRepository.save(user);
+        String token=jwtUtil.generateToken(user);
+        return new AuthResponse(token,user.getRole().name(), user.getName());
+    }
+
+    public AuthResponse login(LoginRequest request)
+    {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),request.getPassword())
+        );
+
+        User user=(User) userRepository.findByEmail(request.getEmail())
+                .orElseThrow(()->new RuntimeException("User not found"));
+
+        String token=jwtUtil.generateToken(user);
+        return new AuthResponse(token,user.getRole().name(), user.getName());
+    }
+}
